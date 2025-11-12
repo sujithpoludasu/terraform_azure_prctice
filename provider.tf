c452a7f8dc0ec8292da178c9bc45a5a2843edd06
admin <sujithram.polidas@gmail.com>
2025-11-12 18:43:25 +0530

    class1

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.52.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "COMMON_RG1"
+      storage_account_name = "azureb555storage"
+      container_name       = "azureb55storagecontainer"
+      key                  = "terraform_terrclass1.tfstate"
+  }
 }
 
 provider "azurerm" {
--- resource_group.tf
+++ resource_group.tf
@@ -7,3 +7,16 @@ resource "azurerm_resource_group" "COMMON_RG1" {
   location = "westus2"
 }
 
+resource "azurerm_storage_account" "azureb555storage" {
+  name                     = "azureb555storage"
+  resource_group_name      = "COMMON_RG1"
+  location                 = "eastus2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "azureb55storagecontainer" {
+  name                  = "azureb55storagecontainer"
+  storage_account_id    = azurerm_storage_account.azureb555storage.id
+  container_access_type = "private"
+}
